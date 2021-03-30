4d2477ed4c987607006b13e0e31be208585da86e
masaya-takasaka <takasakamasaya@gmail.com>
2021-03-30 17:55:21 +0900

    issue4記述直し

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,12 +1,11 @@
 class CalendarsController < ApplicationController
 
-  # １週間のカレンダーと予定が表示されるページ
   def index
     get_week
     @plan = Plan.new
   end
 
-  # 予定の保存
+
   def create
     Plan.create(plan_params)
     redirect_to action: :index
@@ -15,7 +14,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:get_week).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
@@ -24,7 +23,7 @@ class CalendarsController < ApplicationController
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     @todays_date = Date.today
-    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
+    
 
     @week_days = []
 
